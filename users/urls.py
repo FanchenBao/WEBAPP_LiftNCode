f6a4413acda73a58df09d59c07cbd933b984ff86
Fanchen Bao <jonathonbao@gmail.com>
2018-10-26 22:39:45 -0400

    added login functionality

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+''' URL configuration for users app'''
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.html'), name = 'login') # login page
+]
