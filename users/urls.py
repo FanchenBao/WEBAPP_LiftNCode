39f16ca924e190bd2360cf8491f4e0b5e9e0f08a
Fanchen Bao <jonathonbao@gmail.com>
2018-10-27 14:04:37 -0400

    added logout and register functionality

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 ''' URL configuration for users app'''
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name = 'login') # login page
+    path('login/', LoginView.as_view(template_name='users/login.html'), name = 'login'), # login page
+    path('logout/', LogoutView.as_view(next_page = 'learning_notes:index'), name = 'logout'), # logout page
+    path('register/', views.register, name = 'register'), # registering a new user
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def register(request):
+    ''' register a new user'''
+    if request.method != 'POST': # user enter registration page for the first time
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data = request.POST)
+        if form.is_valid():
+            form.save() # save new user
+            # log the new user in automatically
+            username = request.POST['username']
+            password = request.POST['password1']
+            authenticated_user = authenticate(username=username, password=password)
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_notes:index'))
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
+
